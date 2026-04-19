ae77be9655404fa91cf671cdd49090ffee015d74
Pete V <petervaleo@gmail.com>
2026-04-19 03:10:33 -0500

    Add sine/square/saw wave impact sounds for enemies

--- core/src/main/java/lando/systems/ld59/assets/SoundType.java
+++ core/src/main/java/lando/systems/ld59/assets/SoundType.java
@@ -8,6 +8,31 @@ import java.util.EnumMap;
 
 public enum SoundType implements AssetType<Sound> {
     SHOT("shot.wav")
+    ,BLIP("blip1.ogg")
+    ,SINE_C1("sine_c1.wav")
+    ,SINE_D("sine_d.wav")
+    ,SINE_E("sine_e.wav")
+    ,SINE_F("sine_f.wav")
+    ,SINE_G("sine_g.wav")
+    ,SINE_A("sine_a.wav")
+    ,SINE_B("sine_b.wav")
+    ,SINE_C2("sine_c2.wav")
+    ,SQUARE_C1("square_c1.wav")
+    ,SQUARE_D("square_d.wav")
+    ,SQUARE_E("square_e.wav")
+    ,SQUARE_F("square_f.wav")
+    ,SQUARE_G("square_g.wav")
+    ,SQUARE_A("square_a.wav")
+    ,SQUARE_B("square_b.wav")
+    ,SQUARE_C2("square_c2.wav")
+    ,SAW_C1("saw_c1.wav")
+    ,SAW_D("saw_d.wav")
+    ,SAW_E("saw_e.wav")
+    ,SAW_F("saw_f.wav")
+    ,SAW_G("saw_g.wav")
+    ,SAW_A("saw_a.wav")
+    ,SAW_B("saw_b.wav")
+    ,SAW_C2("saw_c2.wav")
     ;
 
     private static final String TAG = SoundType.class.getSimpleName();
--- core/src/main/java/lando/systems/ld59/game/components/EnemyTag.java
+++ core/src/main/java/lando/systems/ld59/game/components/EnemyTag.java
@@ -3,7 +3,9 @@ package lando.systems.ld59.game.components;
 import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.math.MathUtils;
+import lando.systems.ld59.assets.SoundType;
 import lando.systems.ld59.game.Components;
+import lando.systems.ld59.game.signals.AudioEvent;
 
 public class EnemyTag implements Component {
     public final Entity entity;
@@ -43,6 +45,104 @@ public class EnemyTag implements Component {
         // play sound or make particle effect
         var health = Components.get(entity, Health.class);
         health.damage(damage);
+
+        boolean useFancySounds = true;
+        if(useFancySounds){
+            switch (energyColor) {
+                case RED:
+                    float sineVolume = .45f;
+                    switch (MathUtils.random(0, 7)) {
+                        case 0:
+                            AudioEvent.playSound(SoundType.SINE_C1, sineVolume);
+                            break;
+                        case 1:
+                            AudioEvent.playSound(SoundType.SINE_D, sineVolume);
+                            break;
+                        case 2:
+                            AudioEvent.playSound(SoundType.SINE_E, sineVolume);
+                            break;
+                        case 3:
+                            AudioEvent.playSound(SoundType.SINE_F, sineVolume);
+                            break;
+                        case 4:
+                            AudioEvent.playSound(SoundType.SINE_G, sineVolume);
+                            break;
+                        case 5:
+                            AudioEvent.playSound(SoundType.SINE_A, sineVolume);
+                            break;
+                        case 6:
+                            AudioEvent.playSound(SoundType.SINE_B, sineVolume);
+                            break;
+                        case 7:
+                            AudioEvent.playSound(SoundType.SINE_C2, sineVolume);
+                            break;
+                    }
+                    break;
+
+
+                case BLUE:
+                    float squareVolume = .05f;
+                    switch (MathUtils.random(0, 7)) {
+                        case 0:
+                            AudioEvent.playSound(SoundType.SQUARE_C1, squareVolume);
+                            break;
+                        case 1:
+                            AudioEvent.playSound(SoundType.SQUARE_D, squareVolume);
+                            break;
+                        case 2:
+                            AudioEvent.playSound(SoundType.SQUARE_E, squareVolume);
+                            break;
+                        case 3:
+                            AudioEvent.playSound(SoundType.SQUARE_F, squareVolume);
+                            break;
+                        case 4:
+                            AudioEvent.playSound(SoundType.SQUARE_G, squareVolume);
+                            break;
+                        case 5:
+                            AudioEvent.playSound(SoundType.SQUARE_A, squareVolume);
+                            break;
+                        case 6:
+                            AudioEvent.playSound(SoundType.SQUARE_B, squareVolume);
+                            break;
+                        case 7:
+                            AudioEvent.playSound(SoundType.SQUARE_C2, squareVolume);
+                            break;
+                    }
+                    break;
+                case GREEN:
+                    float sawVolume = .125f;
+                    switch (MathUtils.random(0, 7)) {
+                        case 0:
+                            AudioEvent.playSound(SoundType.SAW_C1, sawVolume);
+                            break;
+                        case 1:
+                            AudioEvent.playSound(SoundType.SAW_D, sawVolume);
+                            break;
+                        case 2:
+                            AudioEvent.playSound(SoundType.SAW_E, sawVolume);
+                            break;
+                        case 3:
+                            AudioEvent.playSound(SoundType.SAW_F, sawVolume);
+                            break;
+                        case 4:
+                            AudioEvent.playSound(SoundType.SAW_G, sawVolume);
+                            break;
+                        case 5:
+                            AudioEvent.playSound(SoundType.SAW_A, sawVolume);
+                            break;
+                        case 6:
+                            AudioEvent.playSound(SoundType.SAW_B, sawVolume);
+                            break;
+                        case 7:
+                            AudioEvent.playSound(SoundType.SAW_C2, sawVolume);
+                            break;
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
 }
