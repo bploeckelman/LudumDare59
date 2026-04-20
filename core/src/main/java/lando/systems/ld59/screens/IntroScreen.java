a7593ff303188a5c73e481f4c68b1c0a7d6c591a
Clayton Mays <amays2@wisc.edu>
2026-04-19 23:09:25 -0500

    included dialog box

--- core/src/main/java/lando/systems/ld59/screens/IntroScreen.java
+++ core/src/main/java/lando/systems/ld59/screens/IntroScreen.java
@@ -4,18 +4,25 @@ import com.badlogic.ashley.signals.Listener;
 import com.badlogic.ashley.signals.Signal;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.github.tommyettinger.digital.Stringf;
+import com.github.tommyettinger.textra.TypingLabel;
 import com.kotcrab.vis.ui.widget.VisLabel;
 import lando.systems.ld59.Config;
 import lando.systems.ld59.Flag;
+import lando.systems.ld59.Main;
+import lando.systems.ld59.assets.MusicType;
+import lando.systems.ld59.game.signals.AudioEvent;
 import lando.systems.ld59.game.signals.SignalEvent;
 import lando.systems.ld59.utils.Calc;
 
 public class IntroScreen extends BaseScreen implements Listener<SignalEvent> {
 
-    private static final Color BACKGROUND_COLOR = new Color(.1f, .5f, 1f, 1f);
+    NinePatch   dialogBox;
+    TypingLabel typingLabel;
+    String page1 = "";
 
     // TEMPORARY -----------------------------------------
     private float countdownDurationSecs = 3f;
@@ -25,10 +32,19 @@ public class IntroScreen extends BaseScreen implements Listener<SignalEvent> {
 
     private float accum;
 
+    int currentPage = 0;
+    float transitionAlpha = 0f;
+
     public IntroScreen() {
         this.countdownLabel = new VisLabel(Stringf.format("%.1f", countdownTimer));
+        dialogBox = Main.game.assets.dialogBox;
 
         initializeUI();
+
+        AudioEvent.playMusic(MusicType.MAIN_MUSIC);
+
+        typingLabel = new TypingLabel();
+        typingLabel.setWidth(Config.window_width * .7f);
 
         // Tick the engine for one frame first to get everything initialized
         engine.update(0f);
@@ -42,9 +58,6 @@ public class IntroScreen extends BaseScreen implements Listener<SignalEvent> {
         countdownLabel.setText(Stringf.format("%.1f", countdownTimer));
         countdownTimer = Calc.clampf(countdownTimer - delta, 0, countdownDurationSecs);
 
-        if (!transitioning && countdownTimer <= 0) {
-            transitioning = true;
-
             // TODO: maybe move the cleanup code to a BaseScreen method, or make abstract method in BaseScreen to keep cleanup code together
             // Cleanup ECS stuff from this screen before moving to the next screen/scene
 //            SignalEvent.removeListener(this);
@@ -55,9 +68,6 @@ public class IntroScreen extends BaseScreen implements Listener<SignalEvent> {
 
 //            AudioEvent.stopAllMusic();
 
-            game.setScreen(new GameScreen());
-        }
-
         // Pause for frame-stepping if enabled
         if (Flag.FRAME_STEP.isEnabled()) {
             Config.stepped_frame = Gdx.input.isKeyJustPressed(Input.Keys.NUM_9);
@@ -65,18 +75,40 @@ public class IntroScreen extends BaseScreen implements Listener<SignalEvent> {
                 return;
             }
         }
+        if (Gdx.input.justTouched() && accum > .5f) {
+            accum = 0;
+            var touchPos = new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0);
+            worldCamera.unproject(touchPos);
 
-        engine.update(delta);
+            if (transitionAlpha < 1f) {
+                transitionAlpha = 1f;
+            } else if (!typingLabel.hasEnded()) {
+                typingLabel.skipToTheEnd();
+            } else {
+                currentPage++;
+                if (currentPage == 1) {
+                    game.setScreen(new GameScreen());
+                }
+                if (!transitioning && countdownTimer <= 0) {
+                    transitioning = true;
+                }
+
+            }
+        }
     }
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(BACKGROUND_COLOR);
+        ScreenUtils.clear(.0f, .0f, .1f, 1f);
 
+        batch.enableBlending();
         batch.setProjectionMatrix(windowCamera.combined);
         batch.begin();
-        {
-        }
+        batch.setColor(1f, 1f, 1f, transitionAlpha);
+        dialogBox.draw(batch, windowCamera.viewportWidth / 4, windowCamera.viewportHeight / 4, windowCamera.viewportWidth / 2, windowCamera.viewportHeight / 4);
+
+        typingLabel.draw(batch, 1f);
+
         batch.end();
         batch.setShader(null);
 
