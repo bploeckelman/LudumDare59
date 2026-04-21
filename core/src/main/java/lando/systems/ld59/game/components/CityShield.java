43ae2ade214e154bfa2ad680fba063b63a56fc1e
Doug Graham <dsgraham@gmail.com>
2026-04-20 20:41:05 -0500

    Initialize `CityShield` repair timer with constant value.

--- core/src/main/java/lando/systems/ld59/game/components/CityShield.java
+++ core/src/main/java/lando/systems/ld59/game/components/CityShield.java
@@ -1,10 +1,11 @@
 package lando.systems.ld59.game.components;
 
 import com.badlogic.ashley.core.Component;
+import lando.systems.ld59.game.Constants;
 
 public class CityShield implements Component {
 
-    public float repairTimer = 0f;
+    public float repairTimer = Constants.CITY_SHIELD_REPAIR_TIME;
     public Collider collider;
 
     public CityShield(Collider collider) {
